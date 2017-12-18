629defde21f85b7a92b718d6cc94ea18d9acd05b
Mike Nykytenko <ur7ez@ukr.net>
2017-12-18 16:56:07 +0200

    устранение замечаний: Задача №1: подумайте о варианте с короткими свойствами Задача №3: "В корзине 1 товара на сумму 1 грн."

--- ECMAScript-6/script_17.js
+++ ECMAScript-6/script_17.js
@@ -8,7 +8,12 @@ let someObj = {
     item1: "cap",
     item2: "t-shirt"
 };
+// вариант 1:
 ([someObj.item2, someObj.item1] = Object.values(someObj));
+
+// вариант 2:
+let {item2: item1, item1: item2} = someObj;
+someObj = {item1, item2};
 
 // Задача №2
 // Написать стрелочную функцию - аналог Object.assign | Object.assign(target, src1, src2...)
@@ -32,7 +37,19 @@ myObjectAssign(user, visitor, admin);
 // Написать функцию для вывода содержимого корзины. Функция принимает два параметра: цену за единицу товара и количество товаров, и возвращает её в виде строки "В корзине 2 товара на сумму 3 грн." или "В корзине нет товаров". Использовать значение по умолчанию и строковые шаблоны.
 
 function showBusket(price = undefined, quantity = 0) {
-    return (!quantity || !price) ? `В корзине нет товаров` : `В корзине ${quantity} товара на сумму ${price} грн.`;
+    let goodsSpelling = 'товаров';
+    if (quantity > 0) {
+        let lastDigit = +quantity.toString().substr(-1, 1);
+        let lastDecimal = +quantity.toString().substr(-2, 2);
+        if (lastDigit === 1 && lastDecimal !== 11) {
+            goodsSpelling = 'товар';
+        }
+        if (lastDigit >= 2 && lastDigit <= 4 && (lastDecimal < 10 || lastDecimal > 21)) {
+            goodsSpelling = 'товара';
+        }
+    }
+
+    return (!quantity || !price) ? `В корзине нет товаров` : `В корзине ${quantity} ${goodsSpelling} на сумму ${(price * quantity).toFixed(2)} грн.`;
 }
 
 console.log(showBusket(3, 2));
