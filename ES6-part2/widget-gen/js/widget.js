fc58baadb38bc8aca9e58593fa99981f48eacfd6
Mike Nykytenko <ur7ez@ukr.net>
2017-12-31 02:38:13 +0200

    устранение замечаний: 1) Выкинуть `bgcolor` 2) `1 / (24 * 6)` - madic number. Вынести в переменную с пояснением 3) Хотелось бы видеть больше строковых шаблонов 4) От некоторых глобальных переменных можно избавиться в пользу локальных, используемых в качестве  аргументов функций 5) Используйте обращения к свойствам объектов через точку

--- ES6-part2/widget-gen/js/widget.js
+++ ES6-part2/widget-gen/js/widget.js
@@ -8,7 +8,6 @@ let ajax = (url) => {
         // request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         // request.setRequestHeader('Expires', '' + Date.now() + 10 * 60 * 1000);
         // request.setRequestHeader('Cache-Control', 'max-age=' + 10 * 60);
-
         request.send();
 
         let ctx = request;
@@ -16,9 +15,7 @@ let ajax = (url) => {
             if (ctx.status === 200) {
                 resolve(ctx.responseText);
             } else {
-                let error = new Error(ctx.statusText);
-                error.code = ctx.status;
-                reject(error);
+                reject(ctx.responseText);
             }
         };
         request.onerror = () => {
@@ -28,16 +25,18 @@ let ajax = (url) => {
 };
 
 let getWxData = (event) => {
+    let cookiePrefix = 'wxWidget';
     let wgURL = "https://api.apixu.com/v1/current.json";
     let APIkey = '0a2e760f561341c585703421171412';
-    wgCity = document.querySelector('#city').value;
-    wgLng = document.querySelector('#lang').value;
+    let wgCity = document.querySelector('#city').value;
+    let wgLng = document.querySelector('#lang').value;
+    let wgJSON;
 
-    // Проверяем был ли уже аналогичный запрос (куки хранится в течение последних 10 минут)
-    let cached = Cookies.get(cookiePrefix + '&' + wgCity + '&' + wgLng);
+    // Проверяем, был ли уже аналогичный запрос (куки хранится в течение последних 10 минут)
+    let cached = Cookies.get(`${cookiePrefix}&${wgCity}&${wgLng}`);
     if (cached) {
         wgJSON = JSON.parse(cached);
-        document.querySelector('#wg-json').innerText = doWidget();
+        document.querySelector('#wg-json').innerText = doWidget(wgCity, wgLng, wgJSON);
         console.log('result is from browser cookie cache');
         return;
     }
@@ -45,32 +44,42 @@ let getWxData = (event) => {
     let params = {key: APIkey, q: wgCity, lang: wgLng};
     for (let param in params) {
         wgURL += ~wgURL.indexOf('?') ? '&' : '?';
-        wgURL += param + '=' + encodeURIComponent(params[param]);
+        wgURL += `${param}=${encodeURIComponent(params[param])}`;
     }
     let wxWidget = ajax(wgURL);
 
     wxWidget.then(result => {
         wgJSON = JSON.parse(result);
 
-        // Кэшируем результаты погоды на клиенте на 10 минут
-        // wgJSON['url'] = wgURL;
-        Cookies.set(cookiePrefix + '&' + wgCity + '&' + wgLng, JSON.stringify(wgJSON), 1 / (24 * 6), location.href);
+        // Кэшируем результаты погоды на клиенте
+        let storeTime = 1 / (24 * 6);  // время кэширования - 10 минут, рассчитываем как доля суток (параметр функции кэширования - в сутках)
+        Cookies.set(`${cookiePrefix}&${wgCity}&${wgLng}`, JSON.stringify(wgJSON), storeTime, location.href);
         // заполняем темплейт виджета
-        document.querySelector('#wg-json').innerText = doWidget();
+        document.querySelector('#wg-json').innerText = doWidget(wgCity, wgLng, wgJSON);
         console.log('result is from API-request');
     }, error => {
-        console.error(error);
+        let err = JSON.parse(error);
+        console.error(err.error.code, err.error.message);
+        document.querySelector('#wg-json').innerText = err.error.message;
     });
 };
 
-let doWidget = () => {
+/**
+ *
+ * @param wgCity - город виджета
+ * @param wgLng - язык отображения информации о погоде
+ * @param wgJSON - объект погодных данных
+ * @return {string}
+ */
+let doWidget = (wgCity, wgLng, wgJSON) => {
     // console.log(JSON.stringify(wgJSON));
-
     // формируем оформление виджета
     let wgColor = document.querySelector('INPUT[type="radio"]:checked').value;
-    let wxCondx = wgJSON["current"]["condition"];
+    let wxCondx = wgJSON.current.condition;
+
+    // можно конечно получать локальное имя города из API, но так проще:
     let wgCity_local = document.querySelector('#city').selectedOptions[0].textContent;
-    let wgTemp = wgJSON["current"]["temp_c"];
+    let wgTemp = wgJSON.current.temp_c;
     wgTemp = (wgTemp > 0) ? '+' + wgTemp : wgTemp;
 
     document.querySelector('.widget').style.backgroundColor = toColor(wgColor);
@@ -78,24 +87,20 @@ let doWidget = () => {
     document.querySelector('#wgCityHead span').innerText = wgCity_local;
     let wgElem = document.querySelector('#wx-widget-image');
     // wgElem.setAttribute('src', 'wx_condx/' + wxCondx["icon"].match(/(.+\/64x64\/)(.+)/)[2]);
-    wgElem.setAttribute('src', wxCondx["icon"]);
-    wgElem.setAttribute('alt', 'current weather for ' + wgCity);
-    wgElem.setAttribute('title', wxCondx["text"] + ' (' + wgCity_local + ')');
+    wgElem.setAttribute('src', wxCondx.icon);
+    wgElem.setAttribute('alt', `current weather for ${wgCity}`);
+    wgElem.setAttribute('title', `${wxCondx.text} (${wgCity_local}, ${wgJSON.location.country})`);
     document.querySelector('#wgTemp').firstChild.textContent = wgTemp;
 
     // собственно код для вставки на сторонней HTML-странице:
-    let informerDomHTML = '<div id="wx-widget" data-citylocal="' + wgCity_local
-        + '"><script src="' + hostingURL + '/js/widget2.js?q='
-        + wgCity + '&lang=' + wgLng + '&color=' + wgColor +
-        '" type="module" id="ur7ez"></script></div>';
+    let informerDomHTML = `<div id="wx-widget" data-citylocal="${wgCity_local}"><script src="${hostingURL}/js/widget2.js?q=${wgCity}&lang=${wgLng}&color=${wgColor}" type="module" id="ur7ez"></script></div>`;
     return informerDomHTML.trim();
 };
 
 function toColor(strColor) {
-    return (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test('#' + strColor)) ? '#' + strColor : strColor;
+    return (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(`#${strColor}`)) ? `#${strColor}` : strColor;
 }
 
-let wgJSON, wgCity, wgLng;
-let cookiePrefix = 'wxWidget';
 let hostingURL = 'https://ur7ez.000webhostapp.com/widget-gen';
+
 document.querySelector('.widgetBtn').addEventListener('click', getWxData);
--- ES6-part2/widget-gen/js/widget2.js
+++ ES6-part2/widget-gen/js/widget2.js
@@ -13,9 +13,7 @@ let ajax = (url) => {
             if (ctx.status === 200) {
                 resolve(ctx.responseText);
             } else {
-                let error = new Error(ctx.statusText);
-                error.code = ctx.status;
-                reject(error);
+                reject(ctx.responseText);
             }
         };
         request.onerror = () => {
@@ -25,18 +23,19 @@ let ajax = (url) => {
 };
 
 let getWxData = () => {
+    let cookiePrefix = 'wxWidget_outer';
     let idStamp = document.querySelector('#ur7ez');
     if (!idStamp) {
-        doWidget(true);
+        doWidget({die: true});
         return;
     }
-    wgCity = getParameterByName('q', idStamp.src);
+    let wgCity = getParameterByName('q', idStamp.src);
     let wgLng = getParameterByName('lang', idStamp.src);
-    wgColor = toColor(getParameterByName('color', idStamp.src));
+    let wgColor = toColor(getParameterByName('color', idStamp.src));
+    let wgJSON;
 
     // source for CSS from hosting site
-    srcURL = hostingURL(idStamp.src);
-    // let wgURL = srcURL + '/widget-gen/wxWidget.php?q=' + wgCity + '&lang=' + wgLng;
+    let srcURL = hostingURL(idStamp.src);
 
     // строка запроса на сервер:
     let wgURL = "https://api.apixu.com/v1/current.json";
@@ -44,15 +43,15 @@ let getWxData = () => {
     let params = {key: '0a2e760f561341c585703421171412', q: wgCity, lang: wgLng};
     for (let param in params) {
         wgURL += ~wgURL.indexOf('?') ? '&' : '?';
-        wgURL += param + '=' + encodeURIComponent(params[param]);
+        wgURL += `${param}=${encodeURIComponent(params[param])}`;
     }
 
     // Проверяем был ли уже аналогичный запрос (куки хранится в течение последних 10 минут)
-    let cached = Cookies.get(cookiePrefix + '&' + wgCity + '&' + wgLng);
+    let cached = Cookies.get(`${cookiePrefix}&${wgCity}&${wgLng}`);
     if (cached) {
         wgJSON = JSON.parse(cached);
         console.log('result is from browser cookie cache');
-        doWidget();
+        doWidget({srcURL, wgColor, wgCity, wgJSON});
         return;
     }
 
@@ -60,49 +59,59 @@ let getWxData = () => {
 
     wxWidget.then(result => {
         wgJSON = JSON.parse(result);
-        // Кэшируем результаты погоды на клиенте на 10 минут
-        Cookies.set(cookiePrefix + '&' + wgCity + '&' + wgLng, JSON.stringify(wgJSON), 1 / (24 * 6), location.href);
+        // Кэшируем результаты погоды на клиенте
+        let storeTime = 1 / (24 * 6);  // время кэширования - 10 минут, рассчитываем как доля суток (параметр функции кэширования - в сутках)
+        Cookies.set(`${cookiePrefix}&${wgCity}&${wgLng}`, JSON.stringify(wgJSON), storeTime, location.href);
 
         console.log('result is from API-request:');
-        doWidget();
+        doWidget({srcURL, wgColor, wgCity, wgJSON});
     }, error => {
-        console.error(error);
+        let err = JSON.parse(error);
+        console.error(err.error.code, err.error.message);
+        doWidget({die: true, dieStatus: err.error.message});
     });
 };
 
-let doWidget = (die = false) => {
+/**
+ *
+ * @param params | object
+ *              .srcURL - source for CSS from hosting site
+ *              .wgColor - фоновый цвет виджета
+ *              .wgCity - город виджета
+ *              .wgJSON - объект погодных данных
+ *              .die = false - 'true' to go off
+ */
+let doWidget = ({srcURL, wgColor, wgCity, wgJSON, die = false, dieStatus = "Неверный формат элемента для информера"} = {}) => {
     // console.log(wgJSON);
 
     let widget = document.getElementById('wx-widget');
     if (die) {
-        widget.innerText = "Неверный формат элемента для информера";
+        widget.innerText = dieStatus;
         widget.style.color = 'red';
         return;
     }
+    // добавляем внешний CSS
+    let lnk = document.createElement('link');
+    document.body.appendChild(lnk);
+    lnk.outerHTML = `<link rel="stylesheet" href="${srcURL}/widget-gen/css/wg-style.css">`;  // wgJSON['url']
 
     let wgCity_local = document.querySelector('#wx-widget').dataset['citylocal'];
     // формируем контейнер, оформление и наполнение виджета
     widget.outerHTML = '<div class="widget" id="wx-widget"><div id="wgCityHead"><span></span></div><div class="wgImg"><img id="wx-widget-image" src=""></div><div class="wgTemp"><nobr><span id="wgTemp"><span class="tsp">&nbsp;</span>°C</span></nobr></div></div>';
-    let wxCondx = wgJSON["current"]["condition"];
-    let wgTemp = wgJSON["current"]["temp_c"];
-    wgTemp = (wgTemp > 0) ? '+' + wgTemp : wgTemp;
+    let wxCondx = wgJSON.current.condition;
+    let wgTemp = wgJSON.current.temp_c;
+    wgTemp = (wgTemp > 0) ? `+${wgTemp}` : wgTemp;
 
     document.querySelector('.widget').style.backgroundColor = wgColor;
     document.querySelector('#wgCityHead span').innerText = wgCity_local;
 
     // добавляем и стилизуем иконку погоды:
     let wgElem = document.querySelector('#wx-widget-image');
-    // let imgUrl = srcURL + '/widget-gen/' + 'wx_condx/' + wxCondx["icon"].match(/(.+\/64x64\/)(.+)/)[2];
-    let imgUrl = wxCondx["icon"];  // рисунок берем от провайдера информера
+    let imgUrl = wxCondx.icon;  // рисунок берем от провайдера информера
     wgElem.setAttribute('src', imgUrl);
-    wgElem.setAttribute('alt', 'current weather for ' + wgCity);
-    wgElem.setAttribute('title', wxCondx["text"] + ' (' + wgCity_local + ')');
+    wgElem.setAttribute('alt', `current weather for ${wgCity}`);
+    wgElem.setAttribute('title', `${wxCondx.text} (${wgCity_local}, ${wgJSON.location.country})`);
     document.querySelector('#wgTemp').firstChild.textContent = wgTemp;
-
-    // добавляем внешний CSS
-    let lnk = document.createElement('link');
-    document.body.appendChild(lnk);
-    lnk.outerHTML = '<link rel="stylesheet" href="' + srcURL + '/widget-gen/css/wg-style.css">';  // wgJSON['url']
 };
 
 /**
@@ -113,7 +122,7 @@ let doWidget = (die = false) => {
  */
 function getParameterByName(name, url = window.location.href) {
     name = name.replace(/[\[\]]/g, "\\$&");
-    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    let regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
@@ -121,11 +130,8 @@ function getParameterByName(name, url = window.location.href) {
 }
 
 function toColor(strColor) {
-    return (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test('#' + strColor)) ? '#' + strColor : strColor;
+    return (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(`#${strColor}`)) ? `#${strColor}` : strColor;
 }
-
-let wgJSON, wgCity, wgColor, srcURL;
-let cookiePrefix = 'wxWidget_outer';
 
 let hostingURL = (str) => {
     let url = document.createElement('a');
